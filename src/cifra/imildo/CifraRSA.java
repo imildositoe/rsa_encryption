9468caa1510eaf1e2c1e204035560e07433baa5d
imildositoe <imildositoe@gmail.com>
2021-05-01 02:02:24 +0200

    organized & ready to push

--- src/cifra/imildo/CifraRSA.java
+++ src/cifra/imildo/CifraRSA.java
@@ -12,19 +12,24 @@ public class CifraRSA {
         BigInteger n, d, e;
         int bitlen = 2048;
         //Random choose of two prime numbers p e q
+
         SecureRandom r = new SecureRandom();
         BigInteger p = new BigInteger(bitlen / 2, 100, r);
         BigInteger q = new BigInteger(bitlen / 2, 100, r);
+
         //Compute n = p * q
         n = p.multiply(q);
+
         //Compute the totiente function phi(n) = (p-1)(q-1)
         BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+
         //Escolher one whole number "e" : 1 < e <phi(n) : "e" e phi(n) beign primes to each other
         e = new BigInteger("3");
 
         while (m.gcd(e).intValue() > 1) {
             e = e.add(new BigInteger("2"));
         }
+
         // d beign o multiplicative inverse of "e"
         d = e.modInverse(m);
         System.out.println("p : " + p);
